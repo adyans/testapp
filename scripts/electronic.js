b6a26722624e653a23e0c2884c83b93f4ab2f1bc
blogdino <blogdino@gmail.com>
2013-02-25 23:50:03 +0700

    test

--- scripts/electronic.js
+++ scripts/electronic.js
@@ -1,9 +1,10 @@
 var serviceURL = "http://localhost/esapp/services/";
+serviceURL = "http://dignitymobile.co.id/projects/esapp/services/getproducts.php?t=electronic";
 
 var products;
 
 $(document).ready(function() {
-	$.getJSON('services/getproducts.php?t=electronic', function(data) {
+	$.getJSON(serviceURL, function(data) {
 		products = data.item;
 		$.each(products, function(index, product) {
 			$("#productList").append('<div class="post-product"><img src="'+product.image+'"><p>'+product.title+'</p><br/><p>'+product.description+'</p><div class="klik_button"><a href="detail_klik.html"><img src="images/nav/button_detail.png" /></a></div></div>');
